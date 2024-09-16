a7898cc5e7d41c8e173959d2f9430ed083b9c884
md5502 <m.baniasadi.d@gmail.com>
2024-09-16 14:06:36 +0330

    fox some bugs and add the online doc using swagger

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,30 @@
-
 from django.contrib import admin
 from django.urls import path, include
-
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+from allauth.account.views import confirm_email
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Todo list Api",
-      default_version='v1',
-      description="API documentation description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="your-email@example.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Todo list Api",
+        default_version="v1",
+        description="API documentation description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="your-email@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("admin/", admin.site.urls),
+   
+    path("accounts/", include("dj_rest_auth.urls")),
+	path("accounts/registration/account-confirm-email/<str:key>/", confirm_email),
+    path("accounts/registration/", include("dj_rest_auth.registration.urls")),
+
 ]

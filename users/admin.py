ab22d8eee3783e0f3f77e4751ee65590d6a25e53
md5502 <m.baniasadi.d@gmail.com>
2024-09-17 10:52:35 +0330

    register a class for User model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 from .models import BaseUser
 
-admin.site.register(BaseUser)
+
+@admin.register(BaseUser)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_admin", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("username", "email", "is_active", "is_admin", "is_superuser")
+    list_filter = ("is_active", "is_admin", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+    filter_horizontal = ("groups", "user_permissions")
+
